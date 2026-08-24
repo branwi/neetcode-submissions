6967b3e8beeeb9f36e16343cb7857e66f722a388
Brian Wei <67200110+branwi@users.noreply.github.com>
2026-08-24 17:53:56 -0400

    Add: python-hash-set-basics - submission-0

---
+++ Python Coding Interviews/python-hash-set-basics/submission-0.py
@@ -0,0 +1,35 @@
+from typing import List, Set
+
+
+def build_hash_set(keys: List[str]) -> Set[str]:
+    new_set = set()
+    for s in keys:
+        new_set.add(s)
+    return new_set
+
+
+def check_keys(hash_set: Set[str], keys: List[str]) -> List[bool]:
+    in_set = [_ for _ in range(len(keys))]
+    for s in range(len(keys)):
+        if keys[s] in hash_set:
+            in_set[s] = True
+        else:
+            in_set[s] = False
+    
+    return in_set
+
+
+
+# do not modify below this line
+
+output1 = build_hash_set(["Alice", "Bob", "Charlie"])
+print(type(output1))         # check the type of the output
+print(sorted(list(output1))) # set order is not guaranteed so we need to sort the list
+
+output2 = build_hash_set(["XY", "XX", "YY", "XY", "YX"]) 
+print(type(output2))         # check the type of the output
+print(sorted(list(output2))) # set order is not guaranteed so we need to sort the list
+
+print(check_keys({"Alice", "Bob", "Charlie"}, ["Alice", "Bob", "Charlie", "David"]))
+print(check_keys({'a', 'b', 'c'}, ['a', 'd', 'c']))
+print(check_keys({'a', 'c'}, ['d', 'c']))
